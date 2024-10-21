65d383a6b101102d08b2f3d4d709faa4bfb3fe89
Christian <c.delicaat@hotmail.com>
2024-10-21 15:43:57 +0200

    Added project cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,17 @@ module.exports = {
       backgroundImage: {
         'characters': "url(/public/images/character_mashup.png)"
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100ch', // add required value here
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 
