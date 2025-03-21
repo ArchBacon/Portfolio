d6fcc6a2515890244db4af9eb1b43a8f903e9c01
Chris <cdelicaat.me@gmail.com>
2025-03-22 00:08:57 +0100

    Added scroll call-to-action and fixed scrolling mishaps

--- assets/controllers/icon_controller.js
+++ assets/controllers/icon_controller.js
@@ -6,5 +6,19 @@ export default class extends Controller {
         super.connect();
 
         createIcons({ icons });
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+
+                if (targetElement) {
+                    targetElement.offsetHeight; // This will force the reflow
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+            });
+        });
     }
 }
