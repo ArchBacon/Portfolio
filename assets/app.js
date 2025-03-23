33e99c20bfd339ea8afbf14bef3f5e8752efbd55
Chris <cdelicaat.me@gmail.com>
2025-03-23 23:24:36 +0100

    Small layout fixes

--- assets/app.js
+++ assets/app.js
@@ -21,14 +21,31 @@ import './styles/app.css';
 // Show scrol to top CTA
 const checkScroll = () => {
     const portfolioElement = document.querySelector('#portfolio');
-    const totopCTA = document.getElementById('totop-cta');
-    const portfolioRect = portfolioElement.getBoundingClientRect();
+    if (portfolioElement) {
+        const totopCTA = document.getElementById('totop-cta');
+        const portfolioRect = portfolioElement.getBoundingClientRect();
 
-    if (portfolioRect.top <= window.innerHeight * 0.6) {
-        totopCTA.classList.replace("opacity-0", "opacity-100"); // Show to-top CTA
-    } else {
-        totopCTA.classList.replace("opacity-100", "opacity-0"); // Hide to-top CTA
+        if (portfolioRect.top <= window.innerHeight * 0.6) {
+            totopCTA.classList.replace("opacity-0", "opacity-100"); // Show to-top CTA
+        } else {
+            totopCTA.classList.replace("opacity-100", "opacity-0"); // Hide to-top CTA
+        }
     }
 };
 window.addEventListener('scroll', checkScroll);
 checkScroll();
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.offsetHeight; // This will force the reflow
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
